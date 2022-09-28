8227b3ff9b5e9df3d076195a3104a05c1b3ae72f
DESKTOP-36BD3PV\Leo <packc38@gmail.com>
2022-09-29 00:36:21 +0900

    .

---
+++ play.js
@@ -0,0 +1,11 @@
+var name ="max";
+var age="29";
+var hasHobbies =true;
+
+function sum(userName, userAge, userHooby){
+  return (
+    "name is : "+ userName + "age is :"+userAge+"hobby : "+userHooby
+  );
+}
+
+console.log(sum(name,age,hasHobbies));
